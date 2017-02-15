65ceedd312c25852f00b495be24731ac32b8a1f1
David Sims <davidesims@icloud.com>
2017-02-15 11:49:21 -0800

    added linear regression training.

--- stock_prices.py
+++ stock_prices.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import quandl
 import math
+import numpy as np
+from sklearn import preprocessing, cross_validation, svm
+from sklearn.linear_model import LinearRegression
 
 # Get stock price for Google
 df = quandl.get('WIKI/GOOGL')
@@ -19,10 +22,32 @@ forecast_col = 'Adj. Close'
 # If the label is undefined, set the label to NaN.
 df.fillna(-9999, inplace=True)
 
-# Predict price from 10 days ago.
+# Predict price from 32 days out.
 forecast_out = int(math.ceil(0.01 * len(df)))
 
-# Shift up the close price so we can show the price from 10 days from now
+# Shift up the close price so we can show the price from 32 days from now
 df['label'] = df[forecast_col].shift(-forecast_out)
 
-print(df.head())
+# Drop NaN rows
+df.dropna(inplace=True)
+
+# Convert numpy array and drop label and return new data frame
+X = np.array(df.drop(['label'], 1))
+y = np.array(df['label'])
+
+# Scale X along other values
+X = preprocessing.scale(X)
+
+# Make sure we only have X's where we have values for Y
+y = np.array(df['label'])
+
+# 20% of data should be testing data
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
+
+# Fit is the same as train, score is the same as test
+# Use n_jobs to enable multi-threads
+clf = LinearRegression(n_jobs=-1)
+clf.fit(X_train, y_train)
+accuracy = clf.score(X_test, y_test)
+
+print(accuracy)
